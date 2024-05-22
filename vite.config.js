e42d611eacb42919efef98d6858e8d240584293e
Jessica Jiao <j7jiao@uwaterloo.ca>
2024-05-22 12:00:24 -0400

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Personal-Website/",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory
+  }
 })
